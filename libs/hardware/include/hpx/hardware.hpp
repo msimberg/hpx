f4c5a5fa6ea580296426654c7f0a9b53f281831a
Mikael Simberg <mikael.simberg@iki.fi>
2019-04-18 11:34:02 +0200

    Add hardware module

---
+++ libs/hardware/include/hpx/hardware.hpp
@@ -0,0 +1,13 @@
+//  Copyright (c) 2019 Mikael Simberg
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.HPX.org/LICENSE_1_0.txt)
+
+#ifndef HPX_HARDWARE_HPP
+#define HPX_HARDWARE_HPP
+
+#include <hpx/hardware/bit_manipulation.hpp>
+#include <hpx/hardware/cpuid.hpp>
+#include <hpx/hardware/timestamp.hpp>
+
+#endif
