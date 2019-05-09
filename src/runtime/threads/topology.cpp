eaa965c625662787a8a2582a8799a306b1a73ca9
Mikael Simberg <mikael.simberg@iki.fi>
2019-05-09 15:44:02 +0200

    Make get_thread_affinity_mask_from_lva take void pointer instead of address_type
    Removes dependency on naming headers, and the underlying hwloc call expects a
    void pointer in any case.

--- src/runtime/threads/topology.cpp
+++ src/runtime/threads/topology.cpp
@@ -11,7 +11,6 @@
 #include <hpx/compat/thread.hpp>
 #include <hpx/errors.hpp>
 #include <hpx/runtime.hpp>
-#include <hpx/runtime/naming/address.hpp>
 #include <hpx/runtime/threads/cpu_mask.hpp>
 #include <hpx/runtime/threads/topology.hpp>
 #include <hpx/format.hpp>
@@ -509,10 +508,8 @@ namespace hpx { namespace threads
 
     ///////////////////////////////////////////////////////////////////////////
     mask_type topology::get_thread_affinity_mask_from_lva(
-        naming::address_type lva
-      , error_code& ec
-        ) const
-    { // {{{
+        void const* lva, error_code& ec) const
+    {    // {{{
         if (&ec != &throws)
             ec = make_success_code();
 
@@ -523,11 +520,11 @@ namespace hpx { namespace threads
             std::unique_lock<mutex_type> lk(topo_mtx);
             int ret =
 #if HWLOC_API_VERSION >= 0x00010b06
-                hwloc_get_area_membind(topo, reinterpret_cast<void const*>(lva),
-                    1, nodeset, &policy, HWLOC_MEMBIND_BYNODESET);
+                hwloc_get_area_membind(
+                    topo, lva, 1, nodeset, &policy, HWLOC_MEMBIND_BYNODESET);
 #else
-                hwloc_get_area_membind_nodeset(topo,
-                    reinterpret_cast<void const*>(lva), 1, nodeset, &policy, 0);
+                hwloc_get_area_membind_nodeset(
+                    topo, lva, 1, nodeset, &policy, 0);
 #endif
 
             if (-1 != ret)
