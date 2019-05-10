9d53756e608c133057b61c83311a6717158dafed
Mikael Simberg <mikael.simberg@iki.fi>
2019-05-10 14:55:56 +0200

    Remove unused timing functionality
    block_profiler and time_logger are unused or disabled.

--- src/runtime/threads/threadmanager.cpp
+++ src/runtime/threads/threadmanager.cpp
@@ -31,7 +31,6 @@
 #include <hpx/topology/topology.hpp>
 #include <hpx/util/bind_back.hpp>
 #include <hpx/util/bind_front.hpp>
-#include <hpx/util/block_profiler.hpp>
 #include <hpx/util/detail/yield_k.hpp>
 #include <hpx/hardware/timestamp.hpp>
 #include <hpx/concurrency/itt_notify.hpp>
