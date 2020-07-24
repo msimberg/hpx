10203dfab6a7c019ab75d5d5a98c22b33597e916
Mikael Simberg <mikael.simberg@iki.fi>
2020-07-24 11:56:50 +0200

    Make parallel_executor use thread_pool_executor helpers for bulk execution
    Use regular, (potentially) hierarchical spawning, with tasks assigned to
    cores in a chunked order, i.e. the first k tasks use a hint for the
    first core, the next k tasks use a hint for the second core, and the
    last k tasks use a hint for the last core. This is the same behaviour as
    thread_pool_executor.

--- libs/executors/include/hpx/executors/parallel_executor.hpp
+++ libs/executors/include/hpx/executors/parallel_executor.hpp
@@ -13,27 +13,20 @@
 #include <hpx/allocator_support/internal_allocator.hpp>
 #include <hpx/assert.hpp>
 #include <hpx/async_base/launch_policy.hpp>
-#include <hpx/execution/algorithms/detail/predicates.hpp>
 #include <hpx/execution/detail/async_launch_policy_dispatch.hpp>
 #include <hpx/execution/detail/post_policy_dispatch.hpp>
 #include <hpx/execution/detail/sync_launch_policy_dispatch.hpp>
-#include <hpx/execution/executors/fused_bulk_execute.hpp>
 #include <hpx/execution/executors/static_chunk_size.hpp>
-#include <hpx/execution/traits/is_executor.hpp>
+#include <hpx/execution/executors/thread_pool_executor.hpp>
 #include <hpx/functional/bind_back.hpp>
 #include <hpx/functional/deferred_call.hpp>
 #include <hpx/functional/invoke.hpp>
 #include <hpx/functional/one_shot.hpp>
 #include <hpx/futures/future.hpp>
 #include <hpx/futures/traits/future_traits.hpp>
-#include <hpx/iterator_support/range.hpp>
 #include <hpx/pack_traversal/unwrap.hpp>
 #include <hpx/serialization/serialize.hpp>
-#include <hpx/synchronization/latch.hpp>
 #include <hpx/threading_base/scheduler_base.hpp>
-#include <hpx/threading_base/thread_data.hpp>
-#include <hpx/threading_base/thread_helpers.hpp>
-#include <hpx/threading_base/thread_pool_base.hpp>
 
 #include <algorithm>
 #include <cstddef>
@@ -105,8 +98,6 @@ namespace hpx { namespace parallel { namespace execution {
           , stacksize_(stacksize)
           , schedulehint_(schedulehint)
           , policy_(l)
-          , num_spread_(spread)
-          , num_tasks_(tasks)
         {
         }
 
@@ -119,8 +110,6 @@ namespace hpx { namespace parallel { namespace execution {
           , stacksize_(stacksize)
           , schedulehint_(schedulehint)
           , policy_(l)
-          , num_spread_(spread)
-          , num_tasks_(tasks)
         {
         }
 
@@ -132,8 +121,6 @@ namespace hpx { namespace parallel { namespace execution {
           , stacksize_(threads::thread_stacksize_default)
           , schedulehint_(schedulehint)
           , policy_(l)
-          , num_spread_(spread)
-          , num_tasks_(tasks)
         {
         }
 
@@ -143,16 +130,15 @@ namespace hpx { namespace parallel { namespace execution {
           , stacksize_(threads::thread_stacksize_default)
           , schedulehint_()
           , policy_(l)
-          , num_spread_(spread)
-          , num_tasks_(tasks)
         {
         }
 
         /// \cond NOINTERNAL
         bool operator==(parallel_policy_executor const& rhs) const noexcept
         {
-            return policy_ == rhs.policy_ && num_spread_ == rhs.num_spread_ &&
-                num_tasks_ == rhs.num_tasks_;
+            return policy_ == rhs.policy_ && priority_ == rhs.priority_ &&
+                stacksize_ == rhs.stacksize_ &&
+                schedulehint_ == rhs.schedulehint_;
         }
 
         bool operator!=(parallel_policy_executor const& rhs) const noexcept
@@ -229,36 +215,11 @@ namespace hpx { namespace parallel { namespace execution {
             typename detail::bulk_function_result<F, S, Ts...>::type>>
         bulk_async_execute(F&& f, S const& shape, Ts&&... ts) const
         {
-            std::size_t num_tasks = num_tasks_;
-            if (num_tasks == std::size_t(-1))
-            {
-                auto pool = threads::detail::get_self_or_default_pool();
-                num_tasks =
-                    (std::min)(std::size_t(128), pool->get_os_thread_count());
-            }
-
-            typedef std::vector<hpx::future<
-                typename detail::bulk_function_result<F, S, Ts...>::type>>
-                result_type;
-
-            result_type results;
-            std::size_t size = hpx::util::size(shape);
-            results.resize(size);
-
-            lcos::local::latch l(size);
-            if (hpx::detail::has_async_policy(policy_))
-            {
-                spawn_hierarchical(results, l, 0, size, num_tasks, f,
-                    hpx::util::begin(shape), ts...);
-            }
-            else
-            {
-                spawn_sequential(
-                    results, l, 0, size, f, hpx::util::begin(shape), ts...);
-            }
-            l.wait();
-
-            return results;
+            auto pool = threads::detail::get_self_or_default_pool();
+            return detail::thread_pool_bulk_async_execute_helper(pool,
+                priority_, stacksize_, schedulehint_, 0,
+                pool->get_os_thread_count(), hierarchical_threshold_, policy_,
+                std::forward<F>(f), shape, std::forward<Ts>(ts)...);
         }
 
         template <typename F, typename S, typename Future, typename... Ts>
@@ -267,93 +228,9 @@ namespace hpx { namespace parallel { namespace execution {
         bulk_then_execute(
             F&& f, S const& shape, Future&& predecessor, Ts&&... ts)
         {
-            using func_result_type =
-                typename detail::then_bulk_function_result<F, S, Future,
-                    Ts...>::type;
-
-            // std::vector<future<func_result_type>>
-            using result_type = std::vector<hpx::future<func_result_type>>;
-
-            auto&& func =
-                detail::make_fused_bulk_async_execute_helper<result_type>(*this,
-                    std::forward<F>(f), shape,
-                    hpx::util::make_tuple(std::forward<Ts>(ts)...));
-
-            // void or std::vector<func_result_type>
-            using vector_result_type =
-                typename detail::bulk_then_execute_result<F, S, Future,
-                    Ts...>::type;
-
-            // future<vector_result_type>
-            using result_future_type = hpx::future<vector_result_type>;
-
-            using shared_state_type =
-                typename hpx::traits::detail::shared_state_ptr<
-                    vector_result_type>::type;
-
-            using future_type = typename std::decay<Future>::type;
-
-            // vector<future<func_result_type>> -> vector<func_result_type>
-            shared_state_type p =
-                lcos::detail::make_continuation_alloc<vector_result_type>(
-                    hpx::util::internal_allocator<>{},
-                    std::forward<Future>(predecessor), policy_,
-                    [func = std::move(func)](future_type&& predecessor) mutable
-                    -> vector_result_type {
-                        // use unwrap directly (instead of lazily) to avoid
-                        // having to pull in dataflow
-                        return hpx::util::unwrap(func(std::move(predecessor)));
-                    });
-
-            return hpx::traits::future_access<result_future_type>::create(
-                std::move(p));
-        }
-        /// \endcond
-
-    protected:
-        /// \cond NOINTERNAL
-        template <typename Result, typename F, typename Iter, typename... Ts>
-        void spawn_sequential(std::vector<hpx::future<Result>>& results,
-            lcos::local::latch& l, std::size_t base, std::size_t size, F&& func,
-            Iter it, Ts&&... ts) const
-        {
-            // spawn tasks sequentially
-            HPX_ASSERT(base + size <= results.size());
-
-            for (std::size_t i = 0; i != size; ++i, ++it)
-            {
-                results[base + i] = async_execute(func, *it, ts...);
-            }
-
-            l.count_down(size);
-        }
-
-        template <typename Result, typename F, typename Iter, typename... Ts>
-        void spawn_hierarchical(std::vector<hpx::future<Result>>& results,
-            lcos::local::latch& l, std::size_t base, std::size_t size,
-            std::size_t num_tasks, F&& func, Iter it, Ts&&... ts) const
-        {
-            if (size > num_tasks)
-            {
-                // spawn hierarchical tasks
-                std::size_t chunk_size = (size + num_spread_) / num_spread_ - 1;
-                chunk_size = (std::max)(chunk_size, num_tasks);
-
-                while (size > chunk_size)
-                {
-                    post([&, base, chunk_size, num_tasks, it] {
-                        spawn_hierarchical(results, l, base, chunk_size,
-                            num_tasks, func, it, ts...);
-                    });
-
-                    base += chunk_size;
-                    it = hpx::parallel::v1::detail::next(it, chunk_size);
-                    size -= chunk_size;
-                }
-            }
-
-            // spawn remaining tasks sequentially
-            spawn_sequential(results, l, base, size, func, it, ts...);
+            return detail::thread_pool_bulk_then_execute_helper(*this, policy_,
+                std::forward<F>(f), shape, std::forward<Future>(predecessor),
+                std::forward<Ts>(ts)...);
         }
         /// \endcond
 
@@ -365,7 +242,7 @@ namespace hpx { namespace parallel { namespace execution {
         void serialize(Archive& ar, const unsigned int version)
         {
             // clang-format off
-            ar & policy_ & num_spread_ & num_tasks_;
+            ar & priority_ & stacksize_ & schedulehint_ & policy_;
             // clang-format on
         }
         /// \endcond
@@ -376,8 +253,7 @@ namespace hpx { namespace parallel { namespace execution {
         threads::thread_stacksize stacksize_;
         threads::thread_schedule_hint schedulehint_;
         Policy policy_;
-        std::size_t num_spread_;
-        std::size_t num_tasks_;
+        static constexpr std::size_t hierarchical_threshold_ = 6;
         /// \endcond
     };
 
