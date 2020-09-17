b8fccf40ef216d54e79126ac14a62d14fc0739c2
aurianer <aurianer@cscs.ch>
2020-09-17 15:26:50 +0200

    Disabling distributed tests/examples with device code

--- hpx/runtime/components/component_factory.hpp
+++ hpx/runtime/components/component_factory.hpp
@@ -39,7 +39,7 @@
 #else
 
 #include <hpx/config.hpp>
-#if defined(HPX_HAVE_DISTRIBUTED_RUNTIME)
+#if defined(HPX_HAVE_DISTRIBUTED_RUNTIME) && !defined(HPX_COMPUTE_DEVICE_CODE)
 #include <hpx/preprocessor/cat.hpp>
 #include <hpx/preprocessor/expand.hpp>
 #include <hpx/preprocessor/nargs.hpp>
