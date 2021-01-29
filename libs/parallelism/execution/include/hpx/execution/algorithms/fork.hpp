0612aa30c381d661ef9267e40fcdcda2b36f0e1f
Mikael Simberg <mikael.simberg@iki.fi>
2021-01-29 11:31:49 +0100

    Add simple CPU thread pool-specific P1897 when_all and fork algorithms
    fork is not proposed in P1897. It's used here as an alternative to a
    shared_future, i.e. it allows splitting a DAG. It starts all work
    eagerly until the predecessor sender, and returns a new sender that
    spawns new tasks from that point onwards (not from the root sender that
    was passed to fork). This may be possible to implement with
    ensure_started instead.

---
+++ libs/parallelism/execution/include/hpx/execution/algorithms/fork.hpp
@@ -0,0 +1,162 @@
+//  Copyright (c) 2020 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+#include <hpx/execution/algorithms/detail/single_result.hpp>
+#include <hpx/execution_base/operation_state.hpp>
+#include <hpx/execution_base/receiver.hpp>
+#include <hpx/execution_base/sender.hpp>
+#include <hpx/synchronization/mutex.hpp>
+#include <hpx/type_support/pack.hpp>
+
+#include <atomic>
+#include <cstddef>
+#include <exception>
+#include <functional>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace hpx { namespace execution { namespace experimental {
+    namespace detail {
+        struct fork_state
+        {
+            hpx::lcos::local::mutex mtx;
+            bool started = false;
+            bool result_ready = false;
+            std::vector<std::function<void()>> continuations;
+        };
+
+        struct fork_receiver
+        {
+            std::shared_ptr<fork_state> st;
+
+            fork_receiver(std::shared_ptr<fork_state> st)
+              : st(st)
+            {
+            }
+
+            template <typename E>
+            void set_error(E&&) noexcept
+            {
+                // TODO
+            }
+
+            void set_done() noexcept {
+                // TODO
+            };
+
+            template <typename... Ts>
+            void set_value(Ts&&... ts) noexcept
+            {
+                // TODO: Too coarse.
+                std::lock_guard<hpx::lcos::local::mutex> l{st->mtx};
+                st->result_ready = true;
+
+                for (auto f : st->continuations)
+                {
+                    f();
+                }
+            }
+        };
+
+        template <typename S, typename Scheduler>
+        struct fork_sender
+        {
+            typename std::decay<S>::type s;
+            typename std::decay<Scheduler>::type scheduler;
+
+            template <template <typename...> class Tuple,
+                template <typename...> class Variant>
+            using value_types =
+                typename hpx::execution::experimental::sender_traits<
+                    S>::template value_types<Tuple, Variant>;
+
+            template <template <typename...> class Variant>
+            using error_types =
+                typename hpx::execution::experimental::sender_traits<
+                    S>::template error_types<Variant>;
+
+            static constexpr bool sends_done = false;
+
+            template <typename R>
+            struct operation_state
+            {
+                std::shared_ptr<fork_state> st;
+                typename std::decay<Scheduler>::type scheduler;
+                typename std::decay<R>::type r;
+
+                void start() noexcept
+                {
+                    auto f = [scheduler = std::move(scheduler),
+                                 r = std::move(r)]() {
+                        // TODO: Error handling?
+                        // TODO: Should this use schedule + connect + start?
+                        // TODO: This could be inline for the first/last
+                        // continuation? If the scheduler is the same?
+                        hpx::execution::experimental::execute(
+                            scheduler, [r = std::move(r)]() mutable {
+                                hpx::execution::experimental::set_value(
+                                    std::move(r));
+                            });
+                    };
+
+                    // TODO: This may be way too coarse.
+                    std::lock_guard<hpx::lcos::local::mutex> l{st->mtx};
+                    if (st->result_ready)
+                    {
+                        // If the result is already available we trigger the
+                        // continuation immediately.
+                        f();
+                    }
+                    else
+                    {
+                        // If the result is not available we add it to the
+                        // shared state which will be triggered when the
+                        // predecessor is ready.
+                        st->continuations.push_back(std::move(f));
+                    }
+                }
+            };
+
+            std::shared_ptr<fork_state> st = std::make_shared<fork_state>();
+
+            // TODO: Make this work for non-void functions.
+            template <typename R>
+            auto connect(R&& r)
+            {
+                // Trigger senders until this point to start.
+                // TODO: This is way to coarse. Do we even need a mutex or could
+                // this be done with atomics/lockfree queue?
+                std::lock_guard<hpx::lcos::local::mutex> l{st->mtx};
+                if (!st->started)
+                {
+                    st->started = true;
+                    hpx::execution::experimental::start(
+                        hpx::execution::experimental::connect(
+                            s, fork_receiver(st)));
+                    // TODO: Passing r to fork_receiver did not work last time I
+                    // tried. What goes wrong?
+                }
+
+                // Then return a new operation state which triggers the
+                // continuations when started.
+                return operation_state<R>{st, scheduler, std::forward<R>(r)};
+            }
+        };
+    }    // namespace detail
+
+    // TODO: This should be a CPO.
+    // TODO: How generic is this implementation?
+    template <typename S, typename Scheduler>
+    auto fork(S&& s, Scheduler&& scheduler)
+    {
+        return detail::fork_sender<S, Scheduler>{
+            std::forward<S>(s), std::forward<Scheduler>(scheduler)};
+    }
+}}}    // namespace hpx::execution::experimental
---
+++ libs/parallelism/execution/include/hpx/execution/algorithms/when_all.hpp
@@ -0,0 +1,135 @@
+//  Copyright (c) 2020 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+#include <hpx/execution/algorithms/detail/single_result.hpp>
+#include <hpx/execution_base/operation_state.hpp>
+#include <hpx/execution_base/receiver.hpp>
+#include <hpx/execution_base/sender.hpp>
+#include <hpx/functional/invoke_fused.hpp>
+#include <hpx/synchronization/mutex.hpp>
+#include <hpx/type_support/pack.hpp>
+
+#include <atomic>
+#include <cstddef>
+#include <exception>
+#include <functional>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace hpx { namespace execution { namespace experimental {
+    namespace detail {
+        struct when_all_state
+        {
+            std::atomic<std::size_t> predecessors_remaining;
+        };
+
+        template <typename R>
+        struct when_all_receiver
+        {
+            std::shared_ptr<when_all_state> st;
+            typename std::decay<R>::type r;
+
+            template <typename R_>
+            when_all_receiver(std::shared_ptr<when_all_state> st, R_&& r)
+              : st(st)
+              , r(std::forward<R>(r))
+            {
+            }
+
+            template <typename E>
+            void set_error(E&& e) noexcept
+            {
+                // TODO: This is most likely not right (no handling of multiple
+                // attached predecessors).
+                hpx::execution::experimental::set_error(
+                    std::move(r), std::forward<E>(e));
+            }
+
+            void set_done() noexcept
+            {
+                // TODO: This is most likely not right (no handling of multiple
+                // attached predecessors).
+                hpx::execution::experimental::set_done(std::move(r));
+            };
+
+            template <typename... Ts>
+            void set_value(Ts&&... ts) noexcept
+            {
+                // TODO: Too coarse.
+                if (--st->predecessors_remaining == 0)
+                {
+                    hpx::execution::experimental::set_value(std::move(r));
+                }
+            }
+        };
+
+        template <typename... Ss>
+        struct when_all_sender
+        {
+            std::tuple<std::decay_t<Ss>...> senders;
+
+            // TODO
+            template <template <typename...> class Tuple,
+                template <typename...> class Variant>
+            using value_types = Variant<Tuple<>>;
+
+            // TODO
+            template <template <typename...> class Variant>
+            using error_types = Variant<>;
+
+            static constexpr bool sends_done = false;
+
+            template <typename OperationStates>
+            struct operation_state
+            {
+                std::decay_t<OperationStates> operation_states;
+
+                void start() noexcept
+                {
+                    hpx::util::invoke_fused(
+                        [](auto... o) {
+                            (...,
+                                hpx::execution::experimental::start(
+                                    std::move(o)));
+                        },
+                        operation_states);
+                }
+            };
+
+            std::shared_ptr<when_all_state> st =
+                std::make_shared<when_all_state>();
+
+            // TODO: Make this work for non-void functions.
+            template <typename R>
+            auto connect(R&& r)
+            {
+                st->predecessors_remaining = sizeof...(Ss);
+                auto operation_states = hpx::util::invoke_fused(
+                    [this, &r](auto... s) {
+                        return std::make_tuple(
+                            hpx::execution::experimental::connect(
+                                std::move(s), when_all_receiver<R>(st, r))...);
+                    },
+                    std::move(senders));
+                return operation_state<decltype(operation_states)>{
+                    std::move(operation_states)};
+            }
+        };
+    }    // namespace detail
+
+    // TODO: This should be a CPO.
+    // TODO: How generic is this implementation?
+    template <typename... Ss>
+    auto when_all(Ss&&... ss)
+    {
+        return detail::when_all_sender<Ss...>{
+            std::make_tuple(std::forward<Ss>(ss)...)};
+    }
+}}}    // namespace hpx::execution::experimental
